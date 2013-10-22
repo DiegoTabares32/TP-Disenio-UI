591a1da8ed0ff21fc6722d882ae516e43e0b0c8d
DiegoTabares32 <diegoexta@hotmail.com>
2013-10-22 19:43:26 -0300

    Fix en HomePuestos

--- src/main/groovy/home/HomePuestos.groovy
+++ src/main/groovy/home/HomePuestos.groovy
@@ -1,5 +1,8 @@
 package home
 
+import java.util.concurrent.ConcurrentSkipListMap.Iter;
+
+import scala.collection.parallel.ParIterableLike.Foreach;
 import dominio.Banda
 import dominio.Butaca
 import dominio.Comprador
@@ -40,13 +43,25 @@ class HomePuestos {
 						new Comprador('Testa','Ferro'), new Espectador ('Jonas', 'Castillo', 23, 'Masculino'),
 						ubicacion, butacas, banda1,	new Noche( '13/09/2013', butacas, banda1)))
 		puestos << puesto
+		puesto = new PuestoDeVenta("1")
+		puesto.agregarEntradaVendida( new Entrada(
+						new Comprador('Diego','Roma'), new Espectador ('Melina', 'Miranda', 23, 'Masculino'),
+						ubicacion, butacas, banda1,	new Noche( '13/09/2013', butacas, banda1)))
+		puestos << puesto
 	}
 	
 	def search(String id){
-		PuestoDeVenta puesto =  this.puestos.find{ puesto -> puesto.id == id }
-		
-		if(puesto != null){
-			return puesto.entradasVendidas
+		def puestos =  this.puestos.findAll{ puesto -> puesto.id == id }
+		def entradas = new ArrayList<>()
+		if(puestos != null){			
+			for (puesto in puestos) {
+				for(entrada in puesto.entradasVendidas){
+					entradas << entrada
+				}
+				
+			}
+			return entradas
 		}
+		
 	}
 }
